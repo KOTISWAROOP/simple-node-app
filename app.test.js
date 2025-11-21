059813977a7c88fc8094c3d0484c5cdf4d8490be
kotiswaroop <tadepalli>
2025-11-21 09:34:52 +0530

    CI/CD

--- app.test.js
+++ app.test.js
@@ -6,13 +6,13 @@ const app = require('./app'); // Import your app logic
 describe('GET /', () => {
 
   // Define a specific test case
-  it('should respond with a 200 status code and "Hello World!!! welcome infoblox TO CI/CD PIPELINE."', async () => {
+  it('should respond with a 200 status code and "Hello World!!! welcome TO CI/CD PIPELINE."', async () => {
     // 'request(app)' uses supertest to make a request to our app object
     const response = await request(app).get('/');
 
     // Assertions: Check if the response is what we expect
     expect(response.statusCode).toBe(200);
-    expect(response.text).toBe('Hello World!!! welcome infoblox TO CI/CD PIPELINE.');
+    expect(response.text).toBe('Hello World!!! welcome TO CI/CD PIPELINE.');
   });
 
 });
